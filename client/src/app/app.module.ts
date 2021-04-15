e8af5f1e8a2a850c4bce50244c54abef0c95c47e
Mehedi Hasan <mehedi609@gmail.com>
2021-04-15 20:06:58 +0600

    added nav component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
