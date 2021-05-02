298c04a947ec656600abe573fdb8cbd349f35914
mehedi609 <mehedi609@gmail.com>
2021-05-02 15:33:08 +0600

    implemented deletePhoto functionality

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -74,4 +74,10 @@ export class PhotoEditorComponent implements OnInit {
       });
     });
   }
+
+  deletePhoto(photoId: number): any {
+    this.memberService.deletePhoto(photoId).subscribe(() => {
+      this.member.photos = this.member.photos.filter((p) => p.id !== photoId);
+    });
+  }
 }
